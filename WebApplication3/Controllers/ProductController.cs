cbb86c83933e877b400b60c185e0319c35a782f3
Maniac8288 <maks-bot-5@mail.ru>
2017-01-17 20:41:15 +0200

    Создал ASP.NET MVC

---
+++ WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product()
+            { ProductId = 1, Name="Ручка",Price=25,Category="Канцтовары" 
+
+            });
+            products.Add(new Product()
+            { ProductId = 2,
+                Category = "Алкоголь",
+                Name = "Вино",
+                Price = 149.99
+            });
+            return View(products);
+        }
+    }
+}
